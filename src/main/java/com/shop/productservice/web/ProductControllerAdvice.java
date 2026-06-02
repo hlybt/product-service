98da77f89c322a541a26d7c8eaa1fd2d2b29d6b4
hlybt <hulyabt@gmail.com>
2026-06-02 20:04:10 +0300

    Add product service error handling

---
+++ src/main/java/com/shop/productservice/web/ProductControllerAdvice.java
@@ -0,0 +1,41 @@
+package com.shop.productservice.web;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.shop.productservice.domain.ProductAlreadyExistsException;
+import com.shop.productservice.domain.ProductNotFoundException;
+
+@RestControllerAdvice
+public class ProductControllerAdvice {
+	@ExceptionHandler(ProductNotFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	String productNotFoundHandler(ProductNotFoundException ex) {
+		return ex.getMessage();
+	}
+	
+	@ExceptionHandler(ProductAlreadyExistsException.class)
+	@ResponseStatus(HttpStatus.UNPROCESSABLE_CONTENT)
+	String productAlreadyExistsHandler(ProductAlreadyExistsException ex) {
+		return ex.getMessage();
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+		var errors = new HashMap<String, String>();
+		ex.getBindingResult().getAllErrors().forEach(error -> {
+			String fieldName = ((FieldError) error).getField();
+			String errorMessage = error.getDefaultMessage();
+			errors.put(fieldName, errorMessage);
+		}); 
+		return errors;
+	}
+}
